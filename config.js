db441fc97be5047a9592e57d5005fca628458243
shinezayadoriunhuu99-del <shinezayadoriunhuu99@gmail.com>
2026-08-10 23:58:26 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.AMULET_CONFIG = {
   SUPABASE_URL: "https://hsekahuhqkzoimhwjrli.supabase.co",
-  SUPABASE_ANON_KEY: "ЭНД_PUBLISHABLE_KEY_ЭЭ_ОРУУЛ"
+  SUPABASE_ANON_KEY: "sb_publishable_4w_LoX68lSuY7bH64-PVOA_Xwcfhpkw"
 };
